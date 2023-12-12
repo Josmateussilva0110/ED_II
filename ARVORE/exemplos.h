0794312a49ffa0ef334e09b0fa2cf85e7a182440
Mateus Silva <127849926+Josmateussilva0110@users.noreply.github.com>
2023-12-12 10:02:41 -0300

    Delete ARVORE/exemplos.h

--- ARVORE/exemplos.h
+++
@@ -1,8 +0,0 @@
-typedef struct arvore Arvore;
-Arvore *criar_no(int valor);
-Arvore *inserir(Arvore *no, int valor);
-void exibir(Arvore *no);
-Arvore *tree_min(Arvore *no);
-Arvore *tree_max(Arvore *no);
-Arvore *tree_search(Arvore *x, int k);
-Arvore *tree_sucessor(Arvore *x);
